78eeeebbeb539a1653cc56e982ec7db4e58fec1f
MSCH13 <marcoschinchilla13@gmail.com>
2023-04-19 11:34:27 -0600

    add netlify config

---
+++ dist/70ca70a4d6e800c2136e.js
@@ -0,0 +1,1 @@
+import React from"react";import ReactDOM from"react-dom";import App from"./routes/App";var root=ReactDOM.createRoot(document.getElementById("root"));root.render(React.createElement(React.StrictMode,null,React.createElement(App,null)));
